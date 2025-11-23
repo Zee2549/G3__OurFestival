1424fc46d398f09c4610bb6d2111654973b28198
Phanawat khongkhet <phanawat.kho@dome.tu.ac.th>
2025-11-24 03:59:43 +0700

    style(homepage and booth directory page): add booth directory page link section.
    add margin to category's text in booth directory page.

--- java-script/all-page.js
+++ java-script/all-page.js
@@ -24,3 +24,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function startSlideshow() {
+    const slides = document.querySelectorAll('.slide-img');
+    let currentSlide = 0;
+
+    function nextSlide() {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
+    setInterval(nextSlide, 5000);
+}
+window.onload = startSlideshow;
